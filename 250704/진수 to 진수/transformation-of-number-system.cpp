490a30d9866421de8ea76381d98fbcf8778f2365
dltmdwn0325 <108713915+dltmdwn0325@users.noreply.github.com>
2025-07-04 22:48:51 +0900

    Title: 진수 to 진수, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250704/진수 to 진수/transformation-of-number-system.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int Convert_Decimal(string n, int a) {
+    int num = 0;
+    for (int i=0; i<(int)n.size(); i++) {
+        num = num*a + (n[i]-'0');
+    }
+    return num;
+}
+
+string Convert_B(int n, int b) {
+    int binary[20] = {};
+    int cnt = 0;
+    string ans = "";
+    while (true) {
+        if (n<b) {
+            binary[cnt++] = n;
+            break;
+        }
+        binary[cnt++] = n%2;
+        n /= b;
+    }
+    for (int i=cnt-1; i>=0; i--) {
+        ans += (binary[i]+'0');
+    }
+    return ans;
+}
+
+int main() {
+    int a, b;
+    string n;
+    cin >> a >> b;
+    cin >> n;
+
+    int first = Convert_Decimal(n, a);
+    string result = Convert_B(first, b);
+
+    cout << result;
+
+    return 0;
+}
