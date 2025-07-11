aae74f55e069efcdb675ea74a640447b18ca47a7
dltmdwn0325 <108713915+dltmdwn0325@users.noreply.github.com>
2025-07-11 16:00:57 +0900

    Title: 왔다 갔던 구역 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250711/왔다 갔던 구역 2/area-been-to-and-from2.cpp
+++ 250711/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -1,3 +1,44 @@
+// #include <iostream>
+
+// using namespace std;
+
+// #define MAX_N 200
+
+// int main() {
+//     int n, x;
+//     char dir;
+//     int path[MAX_N] = {0, };
+
+//     cin >> n;
+
+//     int start = 100;
+//     int end;
+//     for (int i = 0; i < n; i++) {
+//         cin >> x >> dir;
+//         if (dir == 'L') {
+//             end = start - x;
+//         }
+//         else {
+//             end = start + x;
+//         }
+//         for (int j=min(start, end); j<max(start, end); j++) {
+//                 path[j]++;
+//             }
+//         start = end;
+//     }
+
+//     int cnt = 0;
+//     for (int i=0; i<MAX_N; i++) {
+//         if (path[i] >= 2) {
+//             cnt++;
+//         }
+//     }
+    
+//     cout << cnt;
+
+//     return 0;
+// }
+
 #include <iostream>
 
 using namespace std;
@@ -17,14 +58,17 @@ int main() {
         cin >> x >> dir;
         if (dir == 'L') {
             end = start - x;
+            for (int j=start; j>end; j--) {
+                path[j]++;
+            }
         }
         else {
             end = start + x;
-        }
-        for (int j=min(start, end); j<max(start, end); j++) {
+            for (int j=start; j<end; j++) {
                 path[j]++;
             }
-        start = end;
+        }
+        start = end+1;
     }
 
     int cnt = 0;
