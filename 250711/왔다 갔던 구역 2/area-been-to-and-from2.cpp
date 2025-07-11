5fb323e9b30af95c55c79f946913a646bac29cb0
dltmdwn0325 <108713915+dltmdwn0325@users.noreply.github.com>
2025-07-11 11:06:49 +0900

    Title: 왔다 갔던 구역 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250711/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+#define MAX_N 200
+
+int main() {
+    int n, x;
+    char dir;
+    int path[MAX_N] = {0, };
+
+    cin >> n;
+
+    int start = 100;
+    int end;
+    for (int i = 0; i < n; i++) {
+        cin >> x >> dir;
+        if (dir == 'L') {
+            end = start - x;
+            for (int j=start; j>end; j--) {
+                path[j]++;
+            }
+        }
+        else {
+            end = start + x;
+            for (int j=start; j<end; j++) {
+                path[j]++;
+            }
+        }
+        start = end;
+    }
+
+    int cnt = 0;
+    for (int i=0; i<=MAX_N; i++) {
+        if (path[i] >= 2) {
+            cnt++;
+        }
+    }
+    
+    cout << cnt;
+
+    return 0;
+}
