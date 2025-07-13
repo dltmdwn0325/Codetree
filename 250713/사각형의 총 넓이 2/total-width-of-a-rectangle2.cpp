1a8ed42aacfa077ea5c3b91efbfec5f095e199b0
dltmdwn0325 <108713915+dltmdwn0325@users.noreply.github.com>
2025-07-13 17:04:39 +0900

    Title: 사각형의 총 넓이 2, Time: 17ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 250713/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+#define MAX_N 2000
+#define OFFSET 1000
+
+int dim[MAX_N][MAX_N] = {0, };
+
+int main() {
+    int N;
+    cin >> N;
+
+    int cur = OFFSET;
+    int result = 0;
+
+    for (int i = 0; i < N; i++) {
+        int x1, y1;
+        int x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+
+        for (int i=x1; i<x2; i++) {
+            for (int j=y1; j<y2; j++) {
+                if (dim[i][j] > 0) continue;
+                dim[i][j]++;
+                result += dim[i][j];
+            }
+        }
+    }
+
+    cout << result;
+
+    return 0;
+}
