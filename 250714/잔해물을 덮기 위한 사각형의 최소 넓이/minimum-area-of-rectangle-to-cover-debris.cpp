4f0c8a7c240283ec6826bc1e359f1f97525b862b
dltmdwn0325 <108713915+dltmdwn0325@users.noreply.github.com>
2025-07-14 00:18:17 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 8ms, Memory: 1MB, Status: Wrong Answer - Codetree

---
+++ 250714/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+#define MAX_N 2000
+#define OFFSET 1000
+
+int x1[2], y1[2];
+int x2[2], y2[2];
+
+int main() {
+    cin >> x1[0] >> y1[0] >> x2[0] >> y2[0];
+    cin >> x1[1] >> y1[1] >> x2[1] >> y2[1];
+
+    int checked[MAX_N][MAX_N];
+
+    for (int i=0; i<2; i++) {
+        x1[i] += OFFSET;
+        x2[i] += OFFSET;
+        y1[i] += OFFSET;
+        y2[i] += OFFSET;
+    }
+
+    for (int i=0; i<2; i++) {
+        for (int j=x1[i]; j<x2[i]; j++) {
+            for (int k=y1[i]; k<y2[i]; k++) {
+                checked[j][k] = i+1;
+            }
+        }
+    }
+
+    int max_x = x1[0], max_y = y1[0];
+    for (int i=x1[0]; i<x2[0]; i++) {
+        for (int j=y1[0]; j<y2[0]; j++) {
+            if (checked[i][j] == 1) {
+                if (i>max_x) max_x = i;
+                if (j>max_y) max_y = j;
+            }
+        }
+    }
+
+    int area = 0;
+    for (int i=x1[0]; i<=max_x; i++) {
+        for (int j=y1[0]; j<=max_y; j++) {
+            area++;
+        }
+    }
+
+    cout << area;
+
+    return 0;
+}
