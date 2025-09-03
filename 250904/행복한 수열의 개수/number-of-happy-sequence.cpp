e5a3a16eafe52074bfa79e8aff5e07451cacf131
dltmdwn0325 <108713915+dltmdwn0325@users.noreply.github.com>
2025-09-04 00:15:07 +0900

    Title: 행복한 수열의 개수, Time: 10ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250904/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> grid(n, vector<int>(n));
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    int answer = 0;
+    for (int i=0; i<n; i++) {
+        int cnt_r = 1;
+        for (int j=0; j<n-1; j++) {
+            if (grid[i][j] == grid[i][j+1]) {
+                cnt_r++;
+            }
+            else cnt_r = 1;
+        }
+        if (cnt_r>=m) answer++;
+    }
+
+    for (int j=0; j<n; j++) {
+        int cnt_c = 1;
+        for (int i=0; i<n-1; i++) {
+            if (grid[i][j] == grid[i+1][j]) {
+                cnt_c++;
+            }
+            else cnt_c = 1;
+        }
+        if (cnt_c>=m) answer++;
+    }
+
+    cout << answer;
+
+    return 0;
+}
